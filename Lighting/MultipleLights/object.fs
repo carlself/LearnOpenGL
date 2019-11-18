cffdbcf52b563a21f4e4415815c74a91dde8829f
carlself <carlwang20150101@gmail.com>
2019-11-18 14:13:01 +0800

    Multiple lights

---
+++ Lighting/MultipleLights/object.fs
@@ -0,0 +1,148 @@
+#version 330 core 
+
+out vec4 FragColor;
+
+struct Material {
+	sampler2D diffuse;
+	sampler2D specular;
+
+	float shininess;
+};
+
+uniform Material material;
+
+struct DirLight {
+	vec3 direction;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform DirLight dirLight;
+
+struct PointLight {
+	vec3 position;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+#define NR_POINT_LIGHTS 4
+PointLight pointLights[NR_POINT_LIGHTS];
+
+struct SpotLight {
+	vec3 position;
+	vec3 direction;
+	float cutoff;
+	float outerCutoff;
+
+	float constant;
+	float linear;
+	float quadratic;
+
+	vec3 ambient;
+	vec3 diffuse;
+	vec3 specular;
+};
+
+uniform SpotLight spotLight;
+
+uniform vec3 viewPos;
+
+in vec3 Normal;
+in vec3 FragPos;
+in vec2 TexCoords;
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir);
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir);
+void main()
+{
+	vec3 outpupt = vec3(0.0);
+	vec3 normal = normalize(Normal);
+	vec3 viewDir = normalize(viewPos - FragPos);
+
+	outpupt += CalcDirLight(dirLight, normal, viewDir);
+
+	for(int i = 0; i < NR_POINT_LIGHTS; i++)
+	{
+		outpupt += CalcPointLight(pointLights[i], normal, FragPos, viewDir);
+	}
+	
+	// outpupt += CalcSpotLight(spotLight, normal, FragPos, viewDir);
+
+	FragColor = vec4(outpupt, 1.0);
+}
+
+vec3 CalcDirLight(DirLight light, vec3 normal, vec3 viewDir)
+{
+	vec3 lightDir = normalize(-light.direction);
+	float diff = max(dot(lightDir, normal), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(reflectDir, viewDir), 0.0), material.shininess);
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	return (ambient + diffuse + specular);
+}
+
+vec3 CalcPointLight(PointLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	float diff = max(dot(lightDir, normal), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(reflectDir, viewDir), 0.0), material.shininess);
+
+	float distance = length(light.position - fragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	return (ambient + diffuse + specular);
+}
+
+vec3 CalcSpotLight(SpotLight light, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+	vec3 lightDir = normalize(light.position - fragPos);
+	float diff = max(dot(lightDir, normal), 0.0);
+
+	vec3 reflectDir = reflect(-lightDir, normal);
+	float spec = pow(max(dot(reflectDir, viewDir), 0.0), material.shininess);
+
+	float distance = length(light.position - fragPos);
+	float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * distance * distance);
+
+	float theta = dot(lightDir, normalize(-light.direction));
+	float eplion = light.cutoff - light.outerCutoff;
+	float intensity = clamp((theta - light.outerCutoff)/eplion, 0.0, 1.0);
+
+	vec3 ambient = light.ambient * vec3(texture(material.diffuse, TexCoords));
+	vec3 diffuse = light.diffuse * vec3(texture(material.diffuse, TexCoords));
+	vec3 specular = light.specular * vec3(texture(material.specular, TexCoords));
+
+	ambient *= attenuation;
+	diffuse *= attenuation;
+	specular *= attenuation;
+
+	diffuse *= intensity;
+	specular *= intensity;
+
+	return (ambient + diffuse + specular);
+}
+
